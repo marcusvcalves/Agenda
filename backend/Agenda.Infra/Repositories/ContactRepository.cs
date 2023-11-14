3d80f0dbc63230f88c685ae571f3fa22c92f7208
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-11-14 18:46:28 -0300

    criados repositories de contatos

---
+++ backend/Agenda.Infra/Repositories/ContactRepository.cs
@@ -0,0 +1,57 @@
+﻿using Agenda.Domain.Entities;
+using Agenda.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda.Infra.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly AgendaDbContext _context;
+
+        public ContactRepository(AgendaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Contact>> GetAllAsync()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+        public async Task<Contact> GetByIdAsync(int id)
+        {
+            return await _context.Contacts.FindAsync(id);
+        }
+        public async Task<Contact> CreateAsync(Contact newContact)
+        {
+            _context.Contacts.Add(newContact);
+            await _context.SaveChangesAsync();
+
+            return newContact;
+        }
+        public async Task UpdateAsync(int id, Contact contact)
+        {
+            var existingContact = await GetByIdAsync(id);
+
+            if (existingContact != null)
+            {
+                existingContact.FullName = existingContact.FullName;
+                existingContact.Email = existingContact.Email;
+                existingContact.PhoneNumber = existingContact.PhoneNumber;
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var contactToDelete = await GetByIdAsync(id);
+
+            if (contactToDelete != null)
+            {
+                _context.Contacts.Remove(contactToDelete);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ backend/Agenda.Infra/Repositories/IContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using Agenda.Domain.Entities;
+
+namespace Agenda.Infra.Repositories
+{
+    public interface IContactRepository
+    {
+        Task<List<Contact>> GetAllAsync();
+        Task<Contact> GetByIdAsync(int id);
+        Task<Contact> CreateAsync(Contact newContact);
+        Task UpdateAsync(int id, Contact contact);
+        Task DeleteAsync(int id);
+    }
+}
