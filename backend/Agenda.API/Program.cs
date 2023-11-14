ebf5bcacd8ce5065208306eb62f3d3dd97be981f
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-11-14 18:48:05 -0300

    adicionados dbcontext, sqlserver e cors

--- backend/Agenda.API/Program.cs
+++ backend/Agenda.API/Program.cs
@@ -1,11 +1,34 @@
+using Agenda.Infra.Data;
+using Agenda.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AgendaDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiDatabase"));
+});
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000", "https://localhost:3000", "http://192.168.0.7:3000")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials();
+                      });
+});
 
 var app = builder.Build();
 
@@ -17,6 +40,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
