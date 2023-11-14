8983028988ce8b316b136ad7865227ca53593a63
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-11-14 18:48:48 -0300

    criada entity de contato

---
+++ backend/Agenda.Domain/Entities/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace Agenda.Domain.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+
+    }
+}
