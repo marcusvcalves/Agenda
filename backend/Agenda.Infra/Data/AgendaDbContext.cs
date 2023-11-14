a058447ef87ac821ea0a81d34d90d85e4f39ff39
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-11-14 18:47:44 -0300

    criado dbcontext

---
+++ backend/Agenda.Infra/Data/AgendaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Agenda.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Agenda.Infra.Data
+{
+    public class AgendaDbContext : DbContext
+    {
+        public AgendaDbContext(DbContextOptions<AgendaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact>? Contacts { get; set; }
+
+    }
+}
